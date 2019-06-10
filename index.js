7c1389b917350057414fbf2b8d37bf444b7f14fd
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-06-10 23:52:08 +0300

    get url and parse it

--- index.js
+++ index.js
@@ -4,9 +4,25 @@
 
 // Зависимости
 const http = require('http');
+const url = require('url');
 
 // сервер будет отвечать на все запросы строкой
 // инициализируем сервер, и слушаем порт 3000
 http.createServer((req, res) => {
+    /**
+     * получаем url и парсим его
+     * @type {Url}
+     */
+    let parsed = url.parse(req.url, true);
+    // получаем путь
+    /**
+     * @var {string} path
+     */
+    let path = parsed.pathname;
+    let trimmed = path.replace(/^\/+|\/+$/g, '');
+    // отправляем ответ
     res.end('Hello World\n');
+    // выводим путь запроса
+    console.log('Запрос принят по маршруту:', trimmed);
+
 }).listen(3000, () => console.log('Сервер слушает порт 3000'));
