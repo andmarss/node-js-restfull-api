298c6310e18f6a4f61e760e0440222d7a5cb8944
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-06-11 14:04:20 +0300

    parse payload

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
  **/
 
 // Зависимости
+/**
+ * @type {module:http}
+ */
 const http = require('http');
+/**
+ * @type {module:url}
+ */
 const url = require('url');
+const StringDescoder = require('string_decoder').StringDecoder;
 
 // сервер будет отвечать на все запросы строкой
 // инициализируем сервер, и слушаем порт 3000
@@ -14,8 +21,8 @@ http.createServer((req, res) => {
      * @type {Url}
      */
     let parsed = url.parse(req.url, true);
-    // получаем путь
     /**
+     * получаем путь
      * @var {string} path
      */
     let path = parsed.pathname;
@@ -29,9 +36,29 @@ http.createServer((req, res) => {
     let headers = req.headers;
     // получаем строку запроса как объект
     let query = parsed.query;
-    // отправляем ответ
-    res.end('Hello World\n');
-    // выводим путь запроса
-    console.log('Запрос принят по маршруту:', trimmed, 'метод запроса:', method, 'параметры строки запроса', query, 'заголовки', headers);
+    // получаем данные, если есть
+    let decoder = new StringDescoder('utf-8');
+    let buffer = '';
+
+    req
+        .on('data', data => buffer += decoder.write(data))
+        .on('end', () => {
+            buffer += decoder.end();
+            // отправляем ответ
+            res.end('Hello World\n');
+            // выводим путь запроса, и прочие данные
+            console.log(
+                'Запрос принят по маршруту:',
+                trimmed,
+                'метод запроса:',
+                method,
+                'параметры строки запроса',
+                query,
+                'заголовки',
+                headers,
+                'данные',
+                buffer
+            );
+        });
 
 }).listen(3000, () => console.log('Сервер слушает порт 3000'));
