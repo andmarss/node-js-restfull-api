1331e9e5d6533d044e7c7329a3c4508d84c47e23
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-06-11 13:36:37 +0300

    parsing headers

--- index.js
+++ index.js
@@ -25,11 +25,13 @@ http.createServer((req, res) => {
      * @var {string} method
      */
     let method = req.method.toLowerCase();
+    // получить http заголовки, как объект
+    let headers = req.headers;
     // получаем строку запроса как объект
     let query = parsed.query;
     // отправляем ответ
     res.end('Hello World\n');
     // выводим путь запроса
-    console.log('Запрос принят по маршруту:', trimmed, 'метод запроса:', method, 'параметры строки запроса', query);
+    console.log('Запрос принят по маршруту:', trimmed, 'метод запроса:', method, 'параметры строки запроса', query, 'заголовки', headers);
 
 }).listen(3000, () => console.log('Сервер слушает порт 3000'));
